c5811ba51716b2e45910abb824e6891b635638e2
MathisDeSousa <mathis.de-sousa@iut-dijon.u-bourgogne.fr>
2025-10-05 15:23:04 +0200

    Update HashService.cs

--- API/Services/HashService.cs
+++ API/Services/HashService.cs
@@ -6,9 +6,9 @@ namespace API.Services
 {
     public class HashService : IHashService
     {
-        public string Sha256Base64(string input)
+        public string Sha256Base64(string? input)
         {
-            if (input == null) throw new ArgumentNullException(nameof(input));
+            input ??= string.Empty;
             using var sha = SHA256.Create();
             byte[] bytes = Encoding.UTF8.GetBytes(input);
             byte[] hash = sha.ComputeHash(bytes);
